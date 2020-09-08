89e32bf1f3aa7c26691cbb726d01d58663fe4709
qifendev <858810078@qq.com>
2020-09-08 15:32:04 +0800

    java interface in Test.java

--- Test.java
+++ Test.java
@@ -1,26 +1,27 @@
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-
 class Test {
-
-    public static void main(String[] args) {
-        try {
-            DatagramSocket datagramSocket = new DatagramSocket(7778);
-            byte[] sendBytes = "hello client".getBytes();
-            byte[] receiveBytes = new byte[1024];
-            DatagramPacket datagramPacket = new DatagramPacket(receiveBytes, receiveBytes.length);
-            datagramSocket.receive(datagramPacket);
-            byte[] data = datagramPacket.getData();
-            InetAddress address = datagramPacket.getAddress();
-            int port = datagramPacket.getPort();
-            datagramSocket.send(new DatagramPacket(sendBytes, sendBytes.length, address, port));
-            System.out.println(new String(data, 0, data.length));
-            datagramSocket.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+    public static void main(final String[] args) {
+        Test3 t = new Test3() {
+            @Override
+            public void doThing() {
+                System.out.println("do thing");
+            }
+        };
+        doInterface(t);
+        doInterface(new Test2());
     }
 
+    public static void doInterface(Test3 test1) {
+        test1.doThing();
+    }
+}
+
+class Test2 implements Test3 {
+    @Override
+    public void doThing() {
+        System.out.println("do thing");
+    }
+}
+
+public interface Test3 {
+    void doThing();
 }
